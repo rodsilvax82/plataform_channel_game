5935a8b7bbd4556769ed94308383e33b6cf388f2
Rodrigo Silva de Carvalho <rodrigo_carvalho82@hotmail.com>
2022-09-12 22:26:11 -0300

    Message

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-final textStyle36  = TextStyle(fontSize: 36, color: Colors.white);
+TextStyle textStyle36 = const TextStyle(fontSize: 36, color: Colors.white);
+TextStyle textStyle75 = const TextStyle(fontSize: 75, color: Colors.white);
